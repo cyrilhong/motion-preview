2ff4cd48cba46393964b8c79f0be8a4b4f64adec
cyrilhong <cyrilhong@gmail.com>
2018-05-15 00:22:42 +0800

    add docs folder

--- gulpfile.js
+++ gulpfile.js
@@ -11,32 +11,32 @@ var cachebust = require('gulp-cache-bust');
 gulp.task('styles', function() {
     gulp.src('sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./app/css/'))
+    .pipe(gulp.dest('./docs/css/'))
     .pipe( livereload( server ));
 });
 
 gulp.task('jade', function() {
     return gulp.src('jade/**/*.jade')
     .pipe(jade()) // pip to jade plugin
-    .pipe(gulp.dest('./app/')) // tell gulp our output folder
+    .pipe(gulp.dest('./docs/')) // tell gulp our output folder
     .pipe( livereload( server ));
 });
 
 gulp.task('image', function() {
-    return gulp.src('app/img/*')
+    return gulp.src('docs/img/*')
     .pipe( livereload( server ));
 });
 
-gulp.src('./app/*/*.html')
+gulp.src('./docs/*/*.html')
     .pipe(cachebust({
         type: 'timestamp'
     }))
-    .pipe(gulp.dest('./app'));
+    .pipe(gulp.dest('./docs'));
 
 gulp.task('watch', function () {
     livereload.listen();
     connect.server({
-        root: ['./app'],
+        root: ['./docs'],
         livereload: true,
         port: 7777,
     });
@@ -46,15 +46,15 @@ gulp.task('watch', function () {
         }
         gulp.watch('sass/**/*.scss',['styles']);
         gulp.watch('jade/*.jade',['jade']);
-        gulp.watch('app/img/**/*.'.src, ['images']);
+        gulp.watch('docs/img/**/*.'.src, ['images']);
     });
 });
 
-gulp.task('app', function(){
-  gulp.src('./app/index.html').pipe(open({uri: 'http://localhost:7777', app: 'Google Chrome'}));
+gulp.task('docs', function(){
+  gulp.src('./docs/index.html').pipe(open({uri: 'http://localhost:7777', app: 'Google Chrome'}));
 });
 
 //Watch task
-gulp.task('default', ['styles','image','jade','watch','app']);
+gulp.task('default', ['styles','image','jade','watch','docs']);
 //Build
 gulp.task('build', ['styles','image','jade']);
